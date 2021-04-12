11b1b498e1f1708f88e5210f091ff5a28a14670d
Jakov10 <jaki.gudelj@yahoo.de>
2021-04-12 19:42:44 +0200

    Answer.ts

---
+++ answer.ts
@@ -0,0 +1,19 @@
+namespace QuizApp {
+
+    export class Answer {
+
+        private _answer: string;
+        private _isBoolean: boolean;
+
+        constructor(_answer: string, _isBoolean: boolean) {
+
+            this._answer = _answer;
+            this._isBoolean = _isBoolean;
+        }
+
+        public showSolution(boolean): void {
+
+        }
+
+    }
+}
